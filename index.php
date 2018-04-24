f3a110b4c8c7be46a2b04327d86598bfbab3327f
Raihan Ihsan Bagaskoro <justbagas@gmail.com>
2018-04-24 08:21:39 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MTT</title>
+</head>
+<body>
+<div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required><BR>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required><BR>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+</body>
+</html>
